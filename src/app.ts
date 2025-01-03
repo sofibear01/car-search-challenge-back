ce7eea1d840af02464740da0071d0eae189513e6
sofibear01 <sofibearzotti01@gmail.com>
2025-01-03 19:31:39 -0300

    url deploy front

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: "http://localhost:5173", // Define explícitamente el origen permitido
+        origin: process.env.FRONT_URL, // Define explícitamente el origen permitido
         credentials: true, // Habilita el envío de cookies o credenciales
         methods: ["GET", "POST", "PUT", "DELETE"], // Define los métodos permitidos
         allowedHeaders: ["Content-Type", "Authorization"], // Define los headers permitidos
